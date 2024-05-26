5a38b0d91ae0e20ff19401ccce364a6735ae4273
jsp-KW <jungsupill@gmail.com>
2024-05-27 00:06:41 +0900

    first_msg

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from todo import todo_router
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+
+
+app = FastAPI()
+
+origins = ["http://127.0.0.1:5501", "http://34.204.152.39"]
+
+app.add_middleware (
+    CORSMiddleware,
+    allow_origins =origins,
+    allow_credentials =True,
+    allow_methods=["*"],
+    allow_headers=["*"],  
+)
+@app.get("/")
+async def welcome() ->dict:
+    return {
+        "msg": "he llo world!!!!!!!!!!!!!"
+    }
+
+app.include_router(todo_router)
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
